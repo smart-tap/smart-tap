6a42b9fb05b198cf12c2a9cd541fc75c42f7b37a
Brian Mulloy <brian@landlessness.net>
2018-10-10 08:56:44 -0400

    added link to cloud server

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 var zetta = require('zetta');
 var Valve = require('./devices/valve.js');
 var Tap = require('./devices/tap.js');
+var LINK_URL = 'http://api.bwmstraining.com';
 
 zetta()
   .name('Sample Tap')
   .use(Valve)
   .use(Tap)
+  .link(LINK_URL)
   .listen(1337, function(){
      console.log('Zetta is running at http://127.0.0.1:1337');
 });

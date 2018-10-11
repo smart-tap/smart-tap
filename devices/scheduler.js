6ce49449da0f2e533284d7b3cd1bbeb1ba6ada01
Brian Mulloy <brian@landlessness.net>
2018-10-11 10:28:04 -0400

    added editable start cycle hour and minute params

--- devices/scheduler.js
+++ devices/scheduler.js
@@ -5,6 +5,9 @@ var Stopwatch = require("statman-stopwatch");
 var Scheduler = module.exports = function() {
 	Device.call(this);
 
+  this.startCycleHour = 6;
+  this.startCycleMinute = 0;
+
   this.elapsedRunningTime = 0;
   this._runningStopwatch = new Stopwatch();
 
@@ -34,13 +37,15 @@ Scheduler.prototype.init = function(config) {
 
   config
       // Define the transitions allowed by the state machine
-    .when('ready', {allow: ['run', 'stop']})
+    .when('ready', {allow: ['run', 'stop', 'updateStartCycleHour', 'updateStartCycleMinute']})
     .when('stopped', {allow: ['run']})
     .when('running', {allow: ['stop']})
 
     // Map the transitions to JavaScript methods
     .map('run', this.run)
     .map('stop', this.stop)
+    .map('updateStartCycleHour', this.updateStartCycleHour, [{ type: 'number', name: 'Start Cycle Hour (ex. 6 for 6am)' }])
+    .map('updateStartCycleMinute', this.updateStartCycleMinute, [{ type: 'number', name: 'Start Cycle Minute (ex. 30 for 6:30am)' }])
 
     .monitor('elapsedRunningTime')
     .monitor('elapsedStoppedTime');
@@ -59,4 +64,14 @@ Scheduler.prototype.stop = function(cb) {
   this._stoppedStopwatch.start();
 	this.state = 'stopped';
 	cb();
+}
+
+Scheduler.prototype.updateStartCycleHour = function(startCycleHour, cb) {
+  this.startCycleHour = startCycleHour;
+  cb();
+}
+
+Scheduler.prototype.updateStartCycleMinute = function(startCycleMinute, cb) {
+  this.startCycleMinute = startCycleMinute;
+  cb();
 }

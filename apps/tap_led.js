6af522f76750fc293a2b56f389091b0e4589e123
Brian Mulloy <brian@landlessness.net>
2018-12-05 02:19:26 -0500

    logic for stagating, sampling and operating LEDs using user LEDs on the bone

--- apps/tap_led.js
+++ apps/tap_led.js
@@ -1,15 +1,34 @@
 module.exports = function(server) {
   var tapQuery = server.where({ type: 'tap' });
-  var operatingLEDQuery = server.where({ type: 'led' });
+  var stagnatingLEDQuery = server.where({ type: 'led', pin: 'USR1' });
+  var samplingLEDQuery = server.where({ type: 'led', pin: 'USR2' });
+  var operatingLEDQuery = server.where({ type: 'led', pin: 'USR3' });
 
-  server.observe([tapQuery, operatingLEDQuery], function(tap, operatingLED){
+  server.observe([tapQuery, stagnatingLEDQuery, samplingLEDQuery, operatingLEDQuery], function(tap, stagnatingLED, samplingLED, operatingLED){
     var tapState = tap.createReadStream('state');
     tapState.on('data', function(newState) {
       switch (newState.data) {
+      case 'stagnating':
+      	stagnatingLED.call('turn-on');
+
+      	samplingLED.call('turn-off');
+      	operatingLED.call('turn-off');
+        break;
+      case 'sampling':
+      	samplingLED.call('turn-on');
+
+      	stagnatingLED.call('turn-off');
+      	operatingLED.call('turn-off');
+        break;
       case 'operating':
       	operatingLED.call('turn-on');
+
+      	samplingLED.call('turn-off');
+      	stagnatingLED.call('turn-off');
         break;
       default:
+      	stagnatingLED.call('turn-off');
+      	samplingLED.call('turn-off');
       	operatingLED.call('turn-off');
         break;
       }

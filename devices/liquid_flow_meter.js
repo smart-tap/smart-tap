3c7f59b766abcc0166ea3da29d954a66fe05955e
Brian Mulloy <brian@landlessness.net>
2018-12-05 23:09:29 -0500

    counting pulses in a 100 ms sample window to create gallons per hour measurment of flow

--- devices/liquid_flow_meter.js
+++ devices/liquid_flow_meter.js
@@ -5,10 +5,19 @@ var util = require('util');
 var Stopwatch = require("statman-stopwatch");
 var bone = require('bonescript');
 
+const SAMPLES_PER_SECOND = 10;
+const MS_PER_S = 1000;
+const S_PER_HOUR = 3600;
+const SAMPLE_INTERVAL = MS_PER_S / SAMPLES_PER_SECOND; // ms
+const ML_PER_PULSE = 2.25;
+const ML_PER_GALLON = 3785.41;
+const GALLON_S_PER_PULSE_HOUR = ML_PER_PULSE / ML_PER_GALLON * S_PER_HOUR;
+
 var LiquidFlowMeter = module.exports = function(pin) {
   Device.call(this);
   this.pin = pin;
-  this.beagleboneInput = 0;
+  this.flow = 0.0; // gallons per hour
+  this._pulses = 0;
 }
 
 util.inherits(LiquidFlowMeter, Device);
@@ -20,19 +29,23 @@ LiquidFlowMeter.prototype.init = function(config) {
     .type('liquid-flow-meter')
     .state('ready')
     .name('LiquidFlowMeter')
-    .monitor('beagleboneInput');
+    .monitor('flow');
 
-  //Everything is off to start
   bone.pinMode(this.pin, bone.INPUT);
-  bone.attachInterrupt(this.pin, true, bone.RISING, measureFlow);
+  bone.attachInterrupt(this.pin, true, bone.RISING, pulseObserved);
 
+  setTimeout(calculateFlow, SAMPLE_INTERVAL);
 }
 
-function measureFlow(beagleboneInput) {
-  // this.beagleboneInput = beagleboneInput;
-  // console.log(JSON.stringify(beagleboneInput));
-  // console.log(JSON.stringify(this.beagleboneInput));
-  // console.log("beagleboneInput: " + beagleboneInput);
-  // console.log("this.beagleboneInput: " + this.beagleboneInput);
+LiquidFlowMeter.prototype.pulseObserved = function(pinInfo) {
   console.log('flow pulse');
+  this._pulses++;
+  console.log('pulses: ' + this._pulses);
+}
+
+LiquidFlowMeter.prototype.calculateFlow = function() {
+  var pulsesPerSecond = this._pulses * SAMPLES_PER_SECOND;
+  this.flow = pulsesPerSecond * GALLON_S_PER_PULSE_HOUR;
+  this._pulses = 0;
+  setTime(calculateFlow, SAMPLE_INTERVAL);
 }

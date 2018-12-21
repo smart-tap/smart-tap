0feb1cfac16b62eaf97eac61d3f4f056ebacca29
Brian Mulloy <brian@landlessness.net>
2018-12-20 23:51:46 -0500

    new pins using perf board

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ if (argv.name) {
 
 var z = zetta()
   .name(serverName)
-  .use(ValveScout, "P8_10")
-  .use(LiquidFlowMeterScout, "P8_7")
+  .use(ValveScout, "P8_18")
+  .use(LiquidFlowMeterScout, "P8_26")
   .use(LED, "P8_14", "P8_16", "P8_18")
   .use(TapScout, argv.tap)
   .use(tapValve)

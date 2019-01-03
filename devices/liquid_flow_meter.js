c6dbf69ec1d4400a30bceadeea0941749a6de210
Demo User <debian@beaglebone.localdomain>
2019-01-03 20:39:10 +0000

    calibrated flow meter

--- devices/liquid_flow_meter.js
+++ devices/liquid_flow_meter.js
@@ -9,7 +9,7 @@ const SAMPLES_PER_SECOND = 10;
 const MS_PER_S = 1000;
 const S_PER_H = 3600;
 const SAMPLE_INTERVAL = MS_PER_S / SAMPLES_PER_SECOND; // ms
-const ML_PER_PULSE = 2.25;
+const ML_PER_PULSE = 2.63;
 const ML_PER_GALLON = 3785.41;
 const GALLON_S_PER_PULSE_H = ML_PER_PULSE / ML_PER_GALLON * S_PER_H;
 
@@ -18,6 +18,7 @@ var LiquidFlowMeter = module.exports = function(pin) {
   this.pin = pin;
   this.flow = 0.0; // gallons per hour
   this._pulses = 0;
+  this.pulseCount = 0;
 }
 
 util.inherits(LiquidFlowMeter, Device);
@@ -29,6 +30,9 @@ LiquidFlowMeter.prototype.init = function(config) {
     .type('liquid-flow-meter')
     .state('ready')
     .name('LiquidFlowMeter')
+    .when('ready', {allow: ['resetPulseCount']})
+    .map('resetPulseCount', this.resetPulseCount)
+    .monitor('pulseCount')
     .monitor('flow');
 
   bone.pinMode(this.pin, bone.INPUT);
@@ -37,8 +41,14 @@ LiquidFlowMeter.prototype.init = function(config) {
   setInterval(this.calculateFlow.bind(this), SAMPLE_INTERVAL);
 }
 
+LiquidFlowMeter.prototype.resetPulseCount = function(cb) {
+  this.pulseCount = 0;
+  cb();
+}
+
 LiquidFlowMeter.prototype.pulseObserved = function(pinInfo) {
   this._pulses++;
+  this.pulseCount++;
 }
 
 LiquidFlowMeter.prototype.calculateFlow = function() {
